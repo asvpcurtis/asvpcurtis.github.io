668f3dbfd55cca8811ab30513d667e26f3170250
Curtis Barlow-Wilkes <curtisw0234@gmail.com>
2016-12-12 21:12:35 -0500

    Update website to contain an image of myself and some content

--- js/script.js
+++ js/script.js
@@ -4,7 +4,19 @@ $(document).ready(function(){
 		{
 			$(this).parent().children().removeClass('active')
 			$(this).addClass('active')
+			$('#swapable').hide();
 		}
 	});
 	$('.copy').text('Copyright © ' + new Date().getFullYear() + ' Curtis Barlow-Wilkes')
 });
+$(function() {
+    $("#mainNavBar").tabs({
+      beforeLoad: function( event, ui ) {
+        ui.jqXHR.fail(function() {
+          ui.panel.html(
+            "Couldn't load this tab. We'll try to fix this as soon as possible. " +
+            "If this wouldn't be a demo." );
+        });
+      }
+    });
+  });
