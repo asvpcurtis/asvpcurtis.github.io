e8b8fca666ca7c9eb05443a9f60acf1c395c85e0
Curtis Barlow-Wilkes <curtisw0234@gmail.com>
2017-01-10 18:16:37 -0500

    Separate project page from main page

--- js/script.js
+++ js/script.js
@@ -1,25 +1,4 @@
 
 $(document).ready(function(){
-  goHome();
-	$('#mainNavBar > ul > li').click(function(){
-		if (!$(this).hasClass('dropdown'))
-		{
-			$(this).parent().children().removeClass('active')
-			$(this).addClass('active')
-		}
-    if ($(this).is('#home')) {goHome();}
-    if ($(this).is('#projects')) {goProjects();}
-	});
 	$('.copy').text('Copyright © ' + new Date().getFullYear() + ' Curtis Barlow-Wilkes')
 });
-
-function goHome()
-{
-  $('.home-content').show()
-  $('.projects-content').hide()
-}
-function goProjects()
-{
-  $('.home-content').hide()
-  $('.projects-content').show()
-}
